912ff9147299c70391d71c7eec3b71aa154475e8
Emilia <66620157+emiliacb@users.noreply.github.com>
2025-04-21 00:46:31 -0300

    Add HomeHandler for temporary home page

--- internal/app/server.go
+++ internal/app/server.go
@@ -1,12 +1,13 @@
 package app
 
 import (
-	"github.com/gin-gonic/gin"
 	"github.com/emiliacb/go-queue-embeddings/internal/api/handlers"
+	"github.com/gin-gonic/gin"
 )
 
 func setupRouter() *gin.Engine {
 	router := gin.Default()
+	router.GET("/", handlers.HomeHandler)
 	router.GET("/health", handlers.HealthHandler)
 	router.POST("/embed", handlers.EmbedHandler)
 	return router
